d48d8df50d8a35045cb50886471a952e6185a101
ZaidanFathan <Goldhair243@gmail.com>
2025-05-06 20:07:15 +0700

    feat: add Services interface for transaction operations

--- src/main/java/org/example/Services.java
+++ src/main/java/org/example/Services.java
@@ -1,6 +1,7 @@
 package org.example;
 
 public interface Services {
-    void setor(Rekening rekening);
-    void createNasabah();
+    void setor(int rekeningId, int jumlahSetoran);
+    void tarikTunai(int rekeningId);
+    void createNasabah(Nasabah nasabah, Rekening rekening);
 }
