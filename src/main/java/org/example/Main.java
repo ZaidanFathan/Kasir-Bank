4696428528ebfb14fd96cf55e745c2cc0f0ca3cc
ZaidanFathan <Goldhair243@gmail.com>
2025-05-06 21:05:54 +0700

    feat: create a menu

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,35 @@
 package org.example;
-
+import java.sql.SQLException;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        try{
+            ServicesOperation servicesOperation = new ServicesOperation();
+            Scanner scanner = new Scanner(System.in);
+
+            while(true) {
+                System.out.println("\n Menu:");
+                System.out.println("1. Setor tunai:");
+                System.out.println("5. Exit:");
+                System.out.print("Choose an option: ");
+                int choice = scanner.nextInt();
+
+                switch (choice){
+                    case 1:
+                        System.out.print("Masukkan nomor rekening : ");
+                        String rekeningId = scanner.next();
+                        System.out.print("Masukkan jumlah uang yang ingin disetorkan : ");
+                        String amount = scanner.next();
+                        servicesOperation.setor(Integer.parseInt(rekeningId),Integer.parseInt(amount));
+                        break;
+
+                    case 5:
+                        System.out.print("Exiting...");
+                        System.exit(0);
+                }
+            }
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
