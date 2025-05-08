4568ab3b5c946637bb55179db1b6c0a1125039d8
ZaidanFathan <Goldhair243@gmail.com>
2025-05-08 09:16:45 +0700

    feat: implement transaksi refactor: add parameter jenis to interface

--- src/main/java/org/example/ServicesOperation.java
+++ src/main/java/org/example/ServicesOperation.java
@@ -3,6 +3,8 @@ package org.example;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 public class ServicesOperation implements Services{
     private Connection connection;
@@ -14,10 +16,18 @@ public class ServicesOperation implements Services{
     @Override
     public void setor(int rekeningId, int jumlahSetoran) {
         String query = "UPDATE rekening SET saldo = saldo + ? WHERE id = ?";
-        try(PreparedStatement stmt = connection.prepareStatement(query)){
+        String query2 = "INSERT INTO transaksi (rekening_id, tipe, jumlah, tanggal) VALUES (?,?,?,?)";
+        try(PreparedStatement stmt = connection.prepareStatement(query);
+        PreparedStatement transactionCreated = connection.prepareStatement(query2)){
             stmt.setInt(1, jumlahSetoran);
             stmt.setInt(2, rekeningId);
             stmt.executeUpdate();
+
+            transactionCreated.setInt(1, rekeningId);
+            transactionCreated.setString(2, "setor");
+            transactionCreated.setInt(3, jumlahSetoran);
+            transactionCreated.setTimestamp(4, Timestamp.valueOf(LocalDateTime.now()));
+            transactionCreated.executeUpdate();
             System.out.print("Saldo berhasil disetor");
         }catch (SQLException e) {
             e.printStackTrace();
@@ -27,10 +37,27 @@ public class ServicesOperation implements Services{
     @Override
     public void tarikTunai(int rekeningId, int jumlahTarikTunai) {
         String query = "UPDATE rekening SET saldo = saldo - ? WHERE id = ?";
+        String query2 = "INSERT INTO transaksi (rekening_id, tipe, jumlah, tanggal) VALUES (?,?,?,?)";
+        try(PreparedStatement stmt = connection.prepareStatement(query);
+            PreparedStatement transactionCreated = connection.prepareStatement(query2)){
+            stmt.setInt(1, jumlahTarikTunai);
+            stmt.setInt(2, rekeningId);
+            stmt.executeUpdate();
+
+            transactionCreated.setInt(1, rekeningId);
+            transactionCreated.setString(2, "tarik tunai");
+            transactionCreated.setInt(3, jumlahTarikTunai);
+            transactionCreated.setTimestamp(4, Timestamp.valueOf(LocalDateTime.now()));
+            transactionCreated.executeUpdate();
+
+            System.out.print("Penarikan tunai berhasil");
+        }catch (SQLException E) {
+            E.printStackTrace();
+        }
     }
 
     @Override
-    public void createNasabah(Rekening rekening) {
+    public void createNasabah(Rekening rekening, String jenis) {
 
     }
 }
