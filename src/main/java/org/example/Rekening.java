2e3e51e71974ee2cbc245a78d4bb0018b8145180
ZaidanFathan <Goldhair243@gmail.com>
2025-05-08 08:28:14 +0700

    feat: create setter getter

--- src/main/java/org/example/Rekening.java
+++ src/main/java/org/example/Rekening.java
@@ -1,17 +1,15 @@
 package org.example;
 
-public class Rekening {
+public abstract class Rekening {
     private int id;
-    private String jenis;
-    private int saldo;
 
-    public Rekening(int id, String jenis, int saldo){
+    public Rekening(int id){
         this.id = id;
-        this.jenis = jenis;
-        this.saldo = saldo;
     }
 
-    public String getJenis() {return jenis;}
-    public int getSaldo() {return saldo;}
+    public int getId() {
+        return id;
+    }
 
+    public abstract double hitungBunga();
 }
--- src/main/java/org/example/Tabungan.java
+++ src/main/java/org/example/Tabungan.java
@@ -1,4 +1,20 @@
 package org.example;
 
-public class Tabungan {
+public class Tabungan extends Rekening{
+    private String jenis;
+    private int saldo;
+
+    public Tabungan(int id, String jenis, int saldo){
+        super(id);
+        this.jenis = jenis;
+        this.saldo = saldo;
+    }
+
+    public String getJenis() {return jenis;}
+    public int getSaldo() {return saldo;}
+
+    @Override
+    public double hitungBunga() {
+        return this.saldo * 0.2;
+    }
 }
